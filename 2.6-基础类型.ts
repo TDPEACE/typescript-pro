54de9fafabfbfbc750911bdec5474b503f4cc6c0
liuqi <qi.liu@cheifclouds.com>
2021-04-06 19:22:10 +0800

    add:类型注解&类型推断

---
+++ 2.6-基础类型.ts
@@ -0,0 +1,29 @@
+
+//基础类型 null undefind symbol boolean viod;
+
+const num: number = 123;
+const teacher: {
+    name: string;
+    age: number;
+} = {
+    name: 'libai',
+    age: 19
+}
+
+const numbers: number[] = [1, 23, 3]
+
+class Person{};
+
+//定义了一个类型为类的的变量
+
+const dell: Person = new Person();
+
+//定义了一个返回结果为number类型的fun ()=>number
+
+const getTotal:()=>number = ()=>{
+    return 123;
+}
+
+
+
+
---
+++ 2.7类型注解类型推断.ts
@@ -0,0 +1,27 @@
+//type annotation 类型注解 :我们来告诉TS变量是什么类型
+
+// type infrence 类型推断  TS 会自动的去尝试分析变量的类型
+
+
+
+
+// let count:number = 123;
+
+let count:number;
+count = 123;
+
+let countInference = 123 ; //===> 推断出let countInference :number 
+
+
+
+const str = '123';
+
+const test  = count + str;
+
+
+
+
+
+
+
+
